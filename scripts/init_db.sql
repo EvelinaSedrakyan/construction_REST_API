befce7bea7b570bf33d8f9c5e6c4669ddc35bb29
Evelina Sedrakyan <evelina.sedrakyan@student.rau.am>
2025-12-30 16:43:44 +0400

    add database initialization script

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,2 +1,4 @@
 CREATE DATABASE construction_db;
-ALTER DATABASE construction_db OWNER TO postgres;
+CREATE USER construction_user WITH PASSWORD 'construction_pass';
+ALTER DATABASE construction_db OWNER TO construction_user;
+GRANT ALL PRIVILEGES ON DATABASE construction_db TO construction_user;
