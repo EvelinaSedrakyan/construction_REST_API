507796ccb3babf3d5ab12ca3c075cae9e50e8951
Evelina Sedrakyan <evelina.sedrakyan@student.rau.am>
2025-12-30 18:05:32 +0400

    Add index on Project.budget

---
+++ alembic/versions/2c964c2f6ba2_add_index_on_project_budget.py
@@ -0,0 +1,21 @@
+"""Add index on project budget
+
+Revision ID: 2c964c2f6ba2
+Revises: f25a8669188e
+Create Date: 2025-12-30 18:04:40.594487
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '2c964c2f6ba2'
+down_revision = 'f25a8669188e'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
