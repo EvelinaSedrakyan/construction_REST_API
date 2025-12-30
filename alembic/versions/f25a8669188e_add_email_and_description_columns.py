9ea1c5117386b38c6f05cd8cb86453fd9d964d22
Evelina Sedrakyan <evelina.sedrakyan@student.rau.am>
2025-12-30 18:03:38 +0400

    Fix Alembic template and add email/description columns

---
+++ alembic/versions/f25a8669188e_add_email_and_description_columns.py
@@ -0,0 +1,21 @@
+"""Add email and description columns
+
+Revision ID: f25a8669188e
+Revises: 
+Create Date: 2025-12-30 18:02:15.875139
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = 'f25a8669188e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
