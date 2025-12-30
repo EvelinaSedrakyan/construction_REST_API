7f42895e2bda8afbe51284b0c186ac45220f2d6c
Evelina Sedrakyan <evelina.sedrakyan@student.rau.am>
2025-12-30 23:07:06 +0400

    Add database initialization script

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -1,4 +1,11 @@
-CREATE DATABASE construction_db;
-CREATE USER construction_user WITH PASSWORD 'construction_pass';
-ALTER DATABASE construction_db OWNER TO construction_user;
+DROP DATABASE IF EXISTS construction_db;
+DROP ROLE IF EXISTS construction_user;
+
+CREATE USER construction_user
+WITH PASSWORD 'construction_password';
+
+CREATE DATABASE construction_db
+    OWNER = construction_user
+    ENCODING = 'UTF8';
+
 GRANT ALL PRIVILEGES ON DATABASE construction_db TO construction_user;
