9fff80bbfbeb5f59588e1e9cacc337e8047acb02
Evelina Sedrakyan <evelina.sedrakyan@student.rau.am>
2025-12-30 18:13:45 +0400

    Add JSON metadata column and GIN index for Project

---
+++ alembic/versions/79b313661682_add_json_metadata_with_gin_index.py
@@ -0,0 +1,21 @@
+"""Add JSON metadata with GIN index
+
+Revision ID: 79b313661682
+Revises: 2c964c2f6ba2
+Create Date: 2025-12-30 18:12:50.394874
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '79b313661682'
+down_revision = '2c964c2f6ba2'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
