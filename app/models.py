3621da2b0bf95b3b81e6b03b5438d15c9fac5cd7
Evelina Sedrakyan <evelina.sedrakyan@student.rau.am>
2025-12-30 23:30:06 +0400

    Add database models

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, JSON
 from sqlalchemy.orm import relationship
 from app.database import Base
 
@@ -7,8 +7,8 @@ class Company(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     country = Column(String, nullable=False)
-    projects = relationship("Project", back_populates="company")
-
+    projects = relationship("Project", back_populates="company", cascade="all delete")
+    workers = relationship("Worker", back_populates="company", cascade="all delete")
 
 class Project(Base):
     __tablename__ = "projects"
@@ -16,16 +16,19 @@ class Project(Base):
     name = Column(String, nullable=False)
     budget = Column(Integer, nullable=False)
     description = Column(String)
+    metadata_json = Column(JSON)
     company_id = Column(Integer, ForeignKey("companies.id"))
     company = relationship("Company", back_populates="projects")
-    workers = relationship("Worker", back_populates="project")
+    workers = relationship("Worker", back_populates="project", cascade="all delete")
 
 
 class Worker(Base):
     __tablename__ = "workers"
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    name = Column(String, nullable=False, index=True)
     role = Column(String, nullable=False)
     email = Column(String)
+    company_id = Column(Integer, ForeignKey("companies.id"))
+    company = relationship("Company", back_populates="workers")
     project_id = Column(Integer, ForeignKey("projects.id"))
     project = relationship("Project", back_populates="workers")
